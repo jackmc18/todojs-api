e58d3358bf6bcbc7ed74947782c1dbd26bcbd11b
Jackmc <jackmc1122@gmail.com>
2019-01-06 10:35:29 -0500

    Add profile, register and signin controller files.

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+
+// const db = knex({
+//   client: 'pg',
+//   connection: config.SMART_BRAIN_DB,
+// });
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('this is working')
 })
+
+// app.get('/', (req, res) => { res.send(database.users) });
+// app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+// app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
 app.listen(3000, () => {
   console.log('running on port 3000')
