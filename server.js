3bd76d5dd7186a7115b0b6a0678b949840776d2f
Jackmc <jackmc1122@gmail.com>
2019-01-06 11:54:00 -0500

    connect the frontend backend and db with signin and register endpoints.

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
+const config = require('./config.json')
 
-// const db = knex({
-//   client: 'pg',
-//   connection: config.SMART_BRAIN_DB,
-// });
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+
+const db = knex({
+  client: 'pg',
+  connection: config.TODOJS_DB,
+});
 
 const app = express();
 
@@ -18,10 +22,15 @@ app.get('/', (req, res) => {
   res.send('this is working')
 })
 
-// app.get('/', (req, res) => { res.send(database.users) });
-// app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
-// app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+db.select('*').from('users').then(data => {
+  console.log(data)
+});
+
+app.get('/', (req, res) => { res.send(database.users) });
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
 app.listen(3000, () => {
   console.log('running on port 3000')
 })
+
