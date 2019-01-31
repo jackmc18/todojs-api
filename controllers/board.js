ec50c23db30ef668aaae55fadc21e1f591fb4d1b
Jackmc <jackmc1122@gmail.com>
2019-01-31 11:21:07 -0500

    Query for the cards related to each list on the board and add cards to the appropriate list when found.

--- controllers/board.js
+++ controllers/board.js
@@ -24,26 +24,30 @@ const handleBoardGet = (req, res, db) => {
     lists: []
   };
 
-  return handleListsGet(req, res, db).then(lists => {
-    lists.map(list => {
-      var tempCards = [];
-      handleCardsGet(req, res, db, list).then(cards => {
-        console.log("returned cards:", cards);
-        tempCards = cards;
+  return handleListsGet(req, res, db)
+    .then(lists => {
+      lists.map(list => {
+        // var tempCards = [];
+        // handleCardsGet(req, res, db, list).then(cards => {
+        //   console.log("returned cards:", cards);
+        //   tempCards = cards;
+        // });
+        // console.log("temp cards:", tempCards);
+        board.lists = [
+          ...board.lists,
+          {
+            listId: list.list_id,
+            listName: list.list_name,
+            cards: []
+          }
+        ];
       });
-      console.log("temp cards:", tempCards);
-      board.lists = [
-        ...board.lists,
-        {
-          listId: list.list_id,
-          listName: list.list_name,
-          cards: []
-        }
-      ];
+      return handleCardsGet(req, res, db, board);
+    })
+    .then(resp => {
+      console.log("board:", resp);
+      res.json(resp);
     });
-    console.log(board);
-    res.json(board);
-  });
 };
 
 const handleListsGet = (req, res, db) => {
@@ -54,6 +58,7 @@ const handleListsGet = (req, res, db) => {
     .where({ board_id: boardId })
     .then(lists => {
       if (lists.length) {
+        console.log("lists:", lists);
         return lists;
       } else {
         res.status(400).json("Not found");
@@ -62,14 +67,26 @@ const handleListsGet = (req, res, db) => {
     .catch(err => res.status(400).json("error getting list"));
 };
 
-const handleCardsGet = (req, res, db, list) => {
+const handleCardsGet = (req, res, db, board) => {
   console.log("getting cards");
+  let listIds = [];
+  board.lists.map(list => {
+    listIds = [...listIds, list.listId];
+  });
   return db
     .select("*")
     .from("cards")
-    .where({ list_id: list.list_id })
+    .whereIn("list_id", listIds)
     .then(cards => {
-      return cards;
+      console.log("cards:", cards);
+      cards.map(card => {
+        board.lists.map(list => {
+          if (list.listId === card.list_id) {
+            list.cards = [...list.cards, card];
+          }
+        });
+      });
+      return board;
     });
 };
 
